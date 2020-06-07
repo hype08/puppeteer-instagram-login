d0f68aaae88d85e15d8531b495021ba16bdc624c
henry <tarou.88@gmail.com>
2020-06-07 21:33:27 +0900

    add connectivity to cells

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
+const { google } = require('googleapis');
+const keys = require("./keys.json")
 const ig = require('./instagram');
+const client = new google.auth.JWT(
+  keys.client_email,
+  null,
+  keys.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets']
+);
+
+client.authorize((err, tokens) => {
+  if (err) {
+    console.info(err);
+    return;
+  } else {
+    gsrun(client)
+    console.info("Connected to Google API..")
+  }
+})
+
+let username;
+let password;
+
+async function gsrun(client) {
+  const gsapi = google.sheets({
+    version: 'v4',
+    auth: client
+  })
+
+  const opt = {
+    spreadsheetId: '1RjauMTNUaQgp09qgwwN_kijdJnuD9ieuI-IvpL831YE',
+    range: 'Sheet1!A2:D'
+  };
+
+  let data = await gsapi.spreadsheets.values.get(opt);
+  let dataArray = data.data.values;
+  username = dataArray[0][0];
+  password = dataArray[0][1];
+  return username, password;
+}
 
 (async() => {
-
   await ig.initialize();
-
-  await ig.login("agilemedia_1", "Login2insta");
-
-
+  await ig.login(username, password);
 })();
--- instagram.js
+++ instagram.js
@@ -1,24 +1,6 @@
 const puppeteer = require('puppeteer');
-const { google } = require('googleapis');
-const keys = require("./keys.json")
+
 const BASE_URL = "https://instagram.com/";
-
-const client = new google.auth.JWT(
-  keys.client_email, 
-  null, 
-  keys.private_key,
-  ['https://www.googleapis.com/auth/spreadsheets']
-);
-
-client.authorize((err, tokens) => {
-  if (err) {
-    console.info(err);
-    return;
-  } else {
-    console.info("Connected to Google API..")
-  }
-
-})
 
 const instagram = {
   browser: null,
@@ -36,7 +18,6 @@ const instagram = {
     // navigate  to instagram main page.
     await instagram.page.goto(BASE_URL, { waitUntil: 'networkidle2' });
 
-
     // // wait for 1sec
     await instagram.page.waitFor(1000);
 
@@ -45,8 +26,6 @@ const instagram = {
     
 
     await instagram.page.type('input[name="password"]', password, { delay: 50 });
-
-    debugger;
 
     // // clicking on the login button 
     loginButton = await instagram.page.$x('//div[contains(text(),"Log In")]');
